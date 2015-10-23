d93a35dece81dd12457bde86a8aea3542a34b1b0
Shamir Alavi <alavi1223@hotmail.com>
2015-10-23 14:55:19 -0400

    Shopicruit problem (working version)

--- Winternship2016/shopicruit.py
+++ Winternship2016/shopicruit.py
@@ -5,45 +5,66 @@ Created on Tue Oct 20 21:13:41 2015
 @author: Shamir
 """
 
-#import re
-import numpy as np
-import os
+import re
 import json
+import numpy as np
+import os      
 
-def storeObject(pattern, content, List):
-    
+
+def returnPattern(pattern, content, temp_array):
+    """
+    """
     for string in pattern.findall(content):
-        List.append(string)
-    return List
-        
-        
-filepath = "C:\\Users\\Shamir\\Desktop\Shopify\\1. Shopicruit problem\\"       # this is the directory where I saved products.json
+        temp_array.append(string)
+    return temp_array        
+
+def storeContent(src_array, dest_array, pattern, content, temp_array):
+    """
+    """
+    for i in range(len(src_array)):
+        returnPattern(pattern, str(content[i]), temp_array)
+        if np.size(temp_array) == 0:
+            pass
+        else:
+            dest_array.append(src_array[i])
+            temp_array = []
+    return dest_array
+    temp_array = []
+
+
+filepath = "C:\\Users\\Shamir\\Desktop\Shopify\\1. Shopicruit problem\\"                               # this is the directory where I saved products.json
 
 try:
-    os.chdir(filepath)                                                          # go to the specified directory
+    os.chdir(filepath)                                                           # go to the specified directory
     file = open('products.json', 'r')  
-except WindowsError:                                                            # if the pathname is invalid
+except WindowsError:                                                           # if the pathname is invalid
     print "Invalid Directory!"
-except IOError as e:                                                            # wrong filename
+except IOError as e:                                                           # wrong filename
     print e
 
+
 file_contents = file.read()
-
-parsed = json.loads(file_contents)
-
-# '(wallet)\"],"(?:variants":)\[{(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":")(?:\w+)(?:","option2":)(?:\w+)(?:,"option3":)(?:\w+,")(?:\w+)\":"(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":)(\w+)\},'
-## all prices
-# '(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":)(?:"\w+"|\w+)(?:,"option2":)(?:"\w+"|\w+)(?:,"option3":)(?:"\w+"|\w+)(?:,"price":")(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":\w+)\}'
-
-#(?(?=regex)then|else)
-#(?(?=product_type":"Wallet)(?:\","tags":\[\))
-#pattern_w = re.compile('(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":)(?:"\w+"|\w+)(?:,"option2":)(?:"\w+"|\w+)(?:,"option3":)(?:"\w+"|\w+)(?:,"price":")(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":\w+)\}', re.I)
+contents = json.loads(file_contents)
+values = np.array(contents.get('products'))                                      # sort product info into individual arrays
 
 
-#pattern_w1 = re.compile('((?<=product_type":"Wallet)(.*)(?=Wallet.png))', re.I)   # 'price":"\d+\.\d+'
-#pattern_l = re.compile("(lamp)", re.I)
-#wallet = []
-#lamp = []
+pattern_w = re.compile("((?<=product_type': u'Wallet)(.))", re.I)
+pattern_l = re.compile("((?<=product_type': u'Lamp)(.))", re.I)
+temp1, temp2, temp3, temp4 = [], [], [], []
+wallets = []
+lamps = []
 
-#storeObject(pattern_w1, file_contents, wallet)
-#storeObject(pattern_l, file_contents, lamp)
+storeContent(values, wallets, pattern_w, values, temp1)
+storeContent(values, lamps, pattern_l, values, temp2)
+
+price = re.compile("price': u'(\d+.\d+)")
+price_wallets = np.array(returnPattern(price, str(wallets), temp3))
+price_lamps   = np.array(returnPattern(price, str(lamps), temp4))
+
+# Calculate total price
+wallet_total = sum(price_wallets.astype(float))
+lamp_total   = sum(price_lamps.astype(float))
+GrandTotal = format(wallet_total + lamp_total, '.2f')
+
+display = 'Total price of all lamps and wallets = $' + GrandTotal
+print display
