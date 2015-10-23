e9d77b316c1354081125dcf65c5e482eef063ae5
Shamir Alavi <alavi1223@hotmail.com>
2015-10-23 14:43:35 -0400

    Shopicruit problem (incomplete regex-only version)
    This commit shows a regex-only tryout (not too wise!)

--- Winternship2016/shopicruit.py
+++ Winternship2016/shopicruit.py
@@ -5,9 +5,10 @@ Created on Tue Oct 20 21:13:41 2015
 @author: Shamir
 """
 
-import re
+#import re
 import numpy as np
 import os
+import json
 
 def storeObject(pattern, content, List):
     
@@ -28,10 +29,21 @@ except IOError as e:                                                            
 
 file_contents = file.read()
 
-pattern_w = re.compile('(wallet)\"],"(variants)', re.I)
-pattern_l = re.compile("(lamp)", re.I)
-wallet = []
-lamp = []
+parsed = json.loads(file_contents)
 
-storeObject(pattern_w, file_contents, wallet)
-storeObject(pattern_l, file_contents, lamp)
+# '(wallet)\"],"(?:variants":)\[{(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":")(?:\w+)(?:","option2":)(?:\w+)(?:,"option3":)(?:\w+,")(?:\w+)\":"(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":)(\w+)\},'
+## all prices
+# '(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":)(?:"\w+"|\w+)(?:,"option2":)(?:"\w+"|\w+)(?:,"option3":)(?:"\w+"|\w+)(?:,"price":")(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":\w+)\}'
+
+#(?(?=regex)then|else)
+#(?(?=product_type":"Wallet)(?:\","tags":\[\))
+#pattern_w = re.compile('(?:"id":)(?:\d+)(?:,"title":")(?:\w+)(?:","option1":)(?:"\w+"|\w+)(?:,"option2":)(?:"\w+"|\w+)(?:,"option3":)(?:"\w+"|\w+)(?:,"price":")(\d+.\d+)\","(?:\w+":\w+\,"compare_at_price":\w+\,"sku":)(?:"\w+","|"",")(?:requires_shipping":\w+,"taxable":\w+,"position":\d+,"product_id":\d+,"created_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","updated_at":"\d+\-\d+\-\w+:\d+:\d+\-\d+:\d+","available":\w+,"featured_image":\w+)\}', re.I)
+
+
+#pattern_w1 = re.compile('((?<=product_type":"Wallet)(.*)(?=Wallet.png))', re.I)   # 'price":"\d+\.\d+'
+#pattern_l = re.compile("(lamp)", re.I)
+#wallet = []
+#lamp = []
+
+#storeObject(pattern_w1, file_contents, wallet)
+#storeObject(pattern_l, file_contents, lamp)
